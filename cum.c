03130ffd81eb01ec690359add27ce6ea13c38ce5
Deltara <50772027+Deltara3@users.noreply.github.com>
2024-10-26 23:49:25 -0400

    we can get girth now

--- cum.c
+++ cum.c
@@ -1,5 +1,35 @@
+#include <stdio.h>
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+    #include <sys/ioctl.h>
+#endif
+
 #include "cum.h"
 
-int main() {
+int get_term_width(void) {
+    // For fuck's sake Microsoft
+    #ifdef _WIN32
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+        if (!GetConsoleScreenBufferInfo(hConsole, &csbi))
+            return -1;
+
+        return csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    #else
+        struct winsize w;
+
+        if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1)
+            return -1;
+
+        return w.ws_col;
+    #endif
+}
+
+int main(void) {
+
+
     return 0;
 }
